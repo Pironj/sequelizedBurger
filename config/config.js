8986616085f3e0c5902bfba8766fca7fa58c2279
Julian Piron <pironj27@gmail.com>
2019-06-09 20:59:33 -0700

    update config

--- config/config.js
+++ config/config.js
@@ -20,7 +20,7 @@ module.exports = {
     dialect: "mysql"
   },
   production: {
-    use_env_variable: JAWSDB_URL,
-    dialect: "mysql"
+    "use_env_variable": "JAWSDB_URL",
+    "dialect": "mysql"
   }
 }
